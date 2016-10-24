2077d77ece29de77885c8cac53ec65e8cf799ca0
Matt Davis <matt_davis@me.com>
2016-10-24 00:18:51 -0500

    Quicksort now uses insertion_sort for small partitions (now can beat .sort() on large arrays)

--- sort/insertion_sort.js
+++ sort/insertion_sort.js
@@ -22,6 +22,27 @@ function insertion_sort (arr, compare=(a,b) => a-b) {
 	return arr;
 }
 
-// let testArr = [8, 4, 7, 2, 9];
-// insertion_sort(testArr, (a,b) => b - a);
-// console.log(testArr);
+// Modified for quicksort use
+function qsort_helper (arr, compare=(a,b) => a-b, lo, hi) {
+	// Starts at 2nd element (1st is already sorted)
+	for (let i = lo + 1; i <= hi; i++) {
+
+		// Store the i-th element, before it is overwritten
+		const x = arr[i];
+		
+		// Start each iteration at the element before arr[i]
+		let j = i - 1;
+
+		// Iterate through elements in reverse order, until the comparison function returns negative
+		while (j >= 0 && compare(arr[j], x) > 0) {
+			arr[j + 1] = arr[j];  // Copy arr[j] -> arr[j + 1] (not a swap)
+			j--;
+		}
+
+		arr[j + 1] = x;  // Assign the original value for arr[i] to its correct insertion point
+	}
+
+	return arr;
+}
+
+module.exports = qsort_helper;
--- sort/quicksort.js
+++ sort/quicksort.js
@@ -1,8 +1,6 @@
-// Future optimizations:
-// - does quicksort need to be called for partitions of size 1?
-// - use of insertion_sort once partition size <= 10
-// - only do median of three if partition size is large (100+ ?)
-// - in-line/one-line swap?
+// Further optimization:
+// - inline swap
+// - try pivoting around lo, like older builds: was faster
 
 'use strict';
 
@@ -12,58 +10,85 @@ Array.prototype.swap = function swap (a, b) {
 	this[b] = temp;
 };
 
+const insertion_sort = require('./insertion_sort.js'),
+      MAX_MEDIAN     = 50,  // Seems to perform best unused
+      MAX_QSORT      = 20;  // 20 seems to be the sweet spot for arrays of length 1M
+
+
 // Partition size = hi - lo
 // Base case: partition size <= 0
 // Recursive case: partition size > 0
-function quicksort (arr, lo=0, hi=arr.length) {
-	// "Median of three" pivot choice optimization (see https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot)
-	// Chooses the pivot index based on the median of the lo, hi, and partition midpoint indexes
-	// Improves worst-case performance (sorted / near-sorted data)
-	// On highly random data, will cause 3-4x performance hit.  Comment out as needed.
-	const mid = Math.floor((hi - lo) / 2) + lo;
-	const median = [lo, mid, hi].sort((a,b) => a-b)[1];
-	arr.swap(lo, median);  // Swaps the "median" into the pivot position (partition() pivots at lo)
+function quicksort (arr, compare, lo=0, hi=arr.length-1) {
+
+	const partSize = hi - lo;
+
+	// Switches to insertion sort for small partitions
+	if (partSize <= MAX_QSORT) {
+		insertion_sort(arr, compare, lo, hi);
 
 
-	// ===== Main algorithm =====
-	// Sort partition {arr[lo] ... arr[hi]} and get new pivot value
-	const pivot = partition(arr, lo, hi);
+	} else {
+		// "Median of three" pivot choice optimization (see https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot)
+		// Chooses the pivot index based on the median of the lo, hi, and partition midpoint values
+		// Improves worst-case performance (sorted / near-sorted data)
+		// On highly random data or small arrays, can cause performance hit.  Comment out as needed.
+		// if (partSize >= MAX_MEDIAN) {
+			const mid = Math.floor((hi - lo) / 2) + lo;
+			const median =
+				(arr[mid] >= arr[lo] && arr[mid] <= arr[hi]) || (arr[mid] >= arr[hi] && arr[mid] <= arr[lo]) ? mid
+				 : ((arr[lo] >= arr[mid] && arr[lo] <= arr[hi]) || (arr[lo] >= arr[hi] && arr[lo] <= arr[mid]) ? lo
+				 : hi
+				);
+			arr.swap(hi, median);  // Swaps the "median" into the pivot position (partition() pivots at hi)
+		// }
 
-	// If the left partition size > 0, sort left partition
-	if (pivot - 1 > lo)    quicksort(arr, lo, pivot - 1);
+		// ===== Main algorithm =====
 
-	// If the right partition size > 0, sort right partition
-	// Void return used for ES6 tail call optimization
-	if (hi > pivot) return quicksort(arr, pivot, hi);
+		// Sort partition {arr[lo] ... arr[hi]} and get new pivot value
+		const pivot = partition(arr, compare, lo, hi);
+
+		// If the left partition size > 1, sort left partition
+		if (pivot - 2 > lo)        quicksort(arr, compare, lo, pivot - 1);
+
+		// If the right partition size > 1, sort right partition
+		// Void return used for ES6 tail call optimization
+		if (hi > pivot + 2) return quicksort(arr, compare, pivot + 1, hi);
+	}
 }
 
 // Sorts the partition {arr[lo] ... arr[hi]} and returns the final index of the pivot value
-function partition (arr, lo, hi) {
-	const pivot = lo;
-	let swapPoint = pivot + 1;  // Swap-to index, starts at first index after pivot
+function partition (arr, compare=(a,b) => a-b, lo, hi) {
+	const pivot = hi;
+	let swapPoint = lo;  // Swap-to index, starts at first index after pivot
 
 	// Iterate from pivot - 1 to end of partition
-	for (let i = swapPoint; i < hi; i++) {
+	for (let i = swapPoint; i < pivot; i++) {
 		// If the current index value is less than the pivot value
-		if (arr[i] < arr[pivot]) {
+		if (compare(arr[pivot], arr[i]) > 0) {  // if (arr[i] < arr[pivot])
+
 			// Swap the current index with the current swap point, then move the swap point forward
 			arr.swap(i, swapPoint);
 			swapPoint++;
 		}
 	}
 
-	// Swap the pivot value to its correct final position (before the final swap point)
-	arr.swap(pivot, swapPoint - 1);
+	// Swap the pivot value to its correct final position (at the final swap point)
+	arr.swap(pivot, swapPoint);
 	return swapPoint;
 }
 
 // let testArr = [];
-// for (let i = 0; i < 100000; i++) {
-// 	while (testArr.length === i) {
-// 		const val = Math.floor(Math.random() * 99999999);
-// 		if (testArr.indexOf(val) === -1) testArr.push(val);
-// 	}
+// for (let i = 0; i < 1000000; i++) {
+	// while (testArr.length === i) {
+	// 	const val = Math.floor(Math.random() * 10000000);
+	// 	if (testArr.indexOf(val) === -1) testArr.push(val);
+	// }
+// 	testArr.push(Math.floor(Math.random() * 10000000));
 // }
+
+// console.log(testArr);
+// quicksort(testArr);
+// console.log(testArr);
 
 // let startTime, diff;
 // startTime = process.hrtime();
@@ -75,3 +100,7 @@ function partition (arr, lo, hi) {
 // diff = process.hrtime(startTime);
 // console.log(diff);
 // console.log(testArr);
+
+// Faster median finder?  Not speed tested yet
+// const median = (arr[mid] >= arr[lo] && arr[mid] <= arr[hi]) || (arr[mid] >= arr[hi] && arr[mid] <= arr[lo]) ? mid : 
+// 	((arr[lo] >= arr[mid] && arr[lo] <= arr[hi]) || (arr[lo] >= arr[hi] && arr[lo] <= arr[mid]) ? lo : hi);
