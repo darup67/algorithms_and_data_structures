2ad17be46803168aa6c4cb3f4858dca1822a8fa4
Matt Davis <matt_davis@me.com>
2016-10-24 20:22:10 -0500

    Minor edits

--- sort/mergesort.js
+++ sort/mergesort.js
@@ -1,25 +1,17 @@
 'use strict';
 
-Array.prototype.swap = function swap (a, b) {
-	const temp = this[a];
-	this[a] = this[b];
-	this[b] = temp;
-};
-
 // Base case:      subarray size <= 1
 // Recursive case: subarray size > 1
-function mergesort (arr, lo=0, hi=arr.length-1) {
-	if (hi > lo) {
-		const mid = Math.floor((hi - lo) / 2) + lo;
-		
-		mergesort(arr, lo, mid);
-		mergesort(arr, mid + 1, hi);
+function mergesort (arr, compare=(a,b)=>a-b, lo=0, hi=arr.length-1) {
+	const mid = Math.floor((hi - lo) / 2) + lo;
 
-		merge(arr, lo, mid, hi);
-	}
+	if (mid > lo) mergesort(arr, compare, lo, mid);
+	if (hi > mid + 1) mergesort(arr, compare, mid + 1, hi);
+
+	merge(arr, compare, lo, mid, hi);
 }
 
-function merge (arr, lo, mid, hi) {
+function merge (arr, compare, lo, mid, hi) {
 	// Stores copy of left subarray so it is not overwritten
 	const buffer   = arr.slice(lo, mid + 1);
 
@@ -34,7 +26,7 @@ function merge (arr, lo, mid, hi) {
 	while (swapPoint <= hi) {
 
 		// If right subarray is used up, OR (left subarray still has unused values AND comparison picks left value)
-		if (rightPoint > hi || (leftPoint < buffer.length) && (buffer[leftPoint] < arr[rightPoint])) {
+		if (rightPoint > hi || (leftPoint < buffer.length && compare(buffer[leftPoint], arr[rightPoint]) < 0)) {
 			arr[swapPoint] = buffer[leftPoint];
 			leftPoint++;
 		
