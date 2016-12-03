9fdea0dff2d09addbd7c8872874475c817834e39
Matt Davis <matt_davis@me.com>
2016-12-03 00:05:14 -0600

    Java: Percolation working on tested inputs

--- java/union_find/Percolation.java
+++ java/union_find/Percolation.java
@@ -1,58 +1,79 @@
 import edu.princeton.cs.algs4.*;
 
 public class Percolation {
-   private boolean[][] grid;
+   private int nSquared;
+   private boolean[][] openGrid;
+   private boolean[][] fullGrid;
+   private WeightedQuickUnionUF uf;
    
    private int xyTo1D(int row, int col) {
-      return ((row - 1) * (grid.length - 1)) + col;
-   }
-   
-   private void outputGrid() {
-      for (int i = 1; i < grid.length; i++) {
-         String line = i + ": ";
-         for (int j = 1; j < grid[i].length; j++) {
-            line += (grid[i][j] ? " open " : "closed") + " ";
-         }
-         StdOut.println(line);
-      }
+      return ((row - 1) * (openGrid.length - 1)) + col;
    }
    
    public Percolation(int n) {
-      int nSquared = n * n;
-      
-      grid = new boolean[n + 1][n + 1];
-      WeightedQuickUnionUF uf = new WeightedQuickUnionUF(nSquared + 2);
+      nSquared = n * n;
+      openGrid = new boolean[n + 1][n + 1];
+      fullGrid = new boolean[n + 1][n + 1];
+      uf = new WeightedQuickUnionUF(nSquared + 2);
       
       // Union top row to virtual top site
       for (int i = 1; i <= n; i++) {
          uf.union(0, i);
       }
-      StdOut.println(uf.connected(0,1));
-      
       // Union bottom row to virtual bottom site
-      for (int i = nSquared - n + 1; i <= nSquared; i++) {
+      /*for (int i = nSquared - n + 1; i <= nSquared; i++) {
          uf.union(i, nSquared + 1);
-      }
-      StdOut.println(uf.connected(nSquared, nSquared + 1));
-      
-      outputGrid();
+      }*/
    }
    
    public void open(int row, int col) {
-      grid[row][col] = true;
-      outputGrid();
+      openGrid[row][col] = true;
+      
+      // Union N site
+      if (row - 1 >= 1              && isOpen(row - 1, col)) {
+         uf.union(xyTo1D(row - 1, col), xyTo1D(row, col));
+      }
+      // Union S site
+      if (row + 1 < openGrid.length && isOpen(row + 1, col)) {
+         uf.union(xyTo1D(row + 1, col), xyTo1D(row, col));
+      }
+      // Union W site
+      if (col - 1 >= 1              && isOpen(row, col - 1)) {
+         uf.union(xyTo1D(row, col - 1), xyTo1D(row, col));
+      }
+      // Union E site
+      if (col + 1 < openGrid.length && isOpen(row, col + 1)) {
+         uf.union(xyTo1D(row, col + 1), xyTo1D(row, col));
+      }
+      
+      if (row == 1)
+         fullGrid[row][col] = true;
+      /*if (row == fullGrid.length && uf.connected(0, xyTo1D(row, col)))
+         fullGrid[row][col] = true;*/
+      
+      for (int i = 2; i < fullGrid.length; i++) {
+         for (int j = 1; j < fullGrid.length; j++) {
+            if (!fullGrid[i][j] && uf.connected(0, xyTo1D(i, j))) {
+               fullGrid[i][j] = true;
+            }
+         }
+      }
    }
    
    public boolean isOpen(int row, int col) {
-      return grid[row][col];
+      return openGrid[row][col];
    }
    
    public boolean isFull(int row, int col) {
-      return !grid[row][col];
+      return fullGrid[row][col];
    }
    
    public boolean percolates() {
-      return true;
+      for (int i = nSquared - openGrid.length + 2; i <= nSquared; i++) {
+         if (uf.connected(0, i)) return true;
+      }
+      return false;
+//      return uf.connected(0, nSquared + 1);
    }
 
    public static void main(String[] args) {
