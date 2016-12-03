040857f09bb1646ad34991f36c9ea5a25aba86b3
Matt Davis <matt_davis@me.com>
2016-12-03 08:04:41 -0600

    Java: Percolation with two UF for visualization, exceptions added

--- java/union_find/Percolation.java
+++ java/union_find/Percolation.java
@@ -1,67 +1,88 @@
 import edu.princeton.cs.algs4.*;
 
 public class Percolation {
-   private int nSquared;             // Grid size (n * n)
-   private boolean[][] openGrid;     // false == closed site, true == open
-   private WeightedQuickUnionUF uf;  // Union-find structure
+   private int n;                        // Grid dimension
+   private int nSquared;                 // Grid size (n * n)
+   private boolean[][] openGrid;         // false == closed site, true == open
+   private WeightedQuickUnionUF uf;      // Union-find for percolates
+   private WeightedQuickUnionUF ufFull;  // Union-find for isFull
    
    // Converts x,y grid reference into UF site reference
    private int xyTo1D(int row, int col) {
       return ((row - 1) * (openGrid.length - 1)) + col;
+   } 
+   
+   // Throws exception if row, col indices are out of range
+   private void validate(int row, int col) {
+      if (row < 1 || col < 1 || row > n || col > n)
+         throw new IndexOutOfBoundsException("row/col index out of bounds");
    }
    
    // Constructor
    public Percolation(int n) {
+      if (n < 1) throw new IllegalArgumentException("error: n less than 1");
+      
+      this.n = n;
       nSquared = n * n;
       openGrid = new boolean[n + 1][n + 1];
-      uf = new WeightedQuickUnionUF(nSquared + 2);
+      uf     = new WeightedQuickUnionUF(nSquared + 2);
+      ufFull = new WeightedQuickUnionUF(nSquared + 2);
       
       // Union top row to virtual top site
       for (int i = 1; i <= n; i++) {
          uf.union(0, i);
+         ufFull.union(0, i);
+      }
+      // Union bottom row to virtual bottom site
+      for (int i = nSquared - n + 1; i <= nSquared; i++) {
+         uf.union(i, nSquared + 1);
       }
    }
    
    // Opens given site and connects it to adjacent open sites
    public void open(int row, int col) {
+      validate(row, col);
       openGrid[row][col] = true;
       
       // Union N site
-      if (row - 1 >= 1              && isOpen(row - 1, col)) {
+      if (row - 1 >= 1 && isOpen(row - 1, col)) {
          uf.union(xyTo1D(row - 1, col), xyTo1D(row, col));
+         ufFull.union(xyTo1D(row - 1, col), xyTo1D(row, col));
       }
       // Union S site
-      if (row + 1 < openGrid.length && isOpen(row + 1, col)) {
+      if (row + 1 <= n && isOpen(row + 1, col)) {
          uf.union(xyTo1D(row + 1, col), xyTo1D(row, col));
+         ufFull.union(xyTo1D(row + 1, col), xyTo1D(row, col));
       }
       // Union W site
-      if (col - 1 >= 1              && isOpen(row, col - 1)) {
+      if (col - 1 >= 1 && isOpen(row, col - 1)) {
          uf.union(xyTo1D(row, col - 1), xyTo1D(row, col));
+         ufFull.union(xyTo1D(row, col - 1), xyTo1D(row, col));
       }
       // Union E site
-      if (col + 1 < openGrid.length && isOpen(row, col + 1)) {
+      if (col + 1 <= n && isOpen(row, col + 1)) {
          uf.union(xyTo1D(row, col + 1), xyTo1D(row, col));
+         ufFull.union(xyTo1D(row, col + 1), xyTo1D(row, col));
       }
    }
    
    // Returns true if a given site is open
    public boolean isOpen(int row, int col) {
+      validate(row, col);
       return openGrid[row][col];
    }
    
    // Returns true if a given site is full
    public boolean isFull(int row, int col) {
-      if (openGrid[row][col] && uf.connected(0, xyTo1D(row, col)))
+      validate(row, col);
+      if (openGrid[row][col] && ufFull.connected(0, xyTo1D(row, col)))
          return true;
       return false;
    }
    
    // Returns true if the system percolates
    public boolean percolates() {
-      for (int i = nSquared - openGrid.length + 2; i <= nSquared; i++) {
-         if (uf.connected(0, i)) return true;
-      }
-      return false;
+      return uf.connected(0, nSquared + 1);
    }
 
    // Main for testing (not implemented)
