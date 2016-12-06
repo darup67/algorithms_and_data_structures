d1ca1ac329eaecc1ad05812a840b899272d6e2a2
Matt Davis <matt_davis@me.com>
2016-12-05 20:28:02 -0600

    Python: quicksort getfromfile, validate added

--- python/sort/quicksort.py
+++ python/sort/quicksort.py
@@ -11,6 +11,7 @@ def quicksort(inputList, lo=0, hi=None):
     pivot, i = lo, lo  ## where i is the index of the last value lower than the pivot
 
     ## Partition: swap all values lower than pivot into the partition [lo + 1 ... i]
+    ## i gets pre-incremented to simplify the swap
     for j in range(lo + 1, hi):
         if inputList[j] < inputList[pivot]:
             i = i + 1
@@ -19,10 +20,24 @@ def quicksort(inputList, lo=0, hi=None):
     ## Swap pivot into ith position
     inputList[pivot], inputList[i] = inputList[i], inputList[pivot]
 
-    ## Recursively sort subarrays to the left and right of pivot
-    quicksort(inputList, lo, i)    ## quicksort from lo to i - 1 (pivot at i)
-    quicksort(inputList, i+1, hi)  ## quicksort from i + 1 to hi
+    ## Recursively sort subarrays to the left and right of pivot (in-place)
+    quicksort(inputList, lo, i)    ## quicksort inputList[lo, i - 1]
+    quicksort(inputList, i+1, hi)  ## quicksort inputList[i + 1, hi]
 
-testList = [49, 4, 3, 22, 7, 45, 2, 6, 11, 9, 17, 1, 37]
+def get_list_from_file(filename):
+    newList, result = list(open(filename)), []
+
+    for item in newList:
+        result.append(int(item.rstrip('\n')))
+    
+    return result
+
+def validate(inputList):
+    for x in range(0, len(inputList) - 1):
+        if inputList[x] > inputList[x + 1]:
+            return False;
+    return True
+
+testList = get_list_from_file('data/QuickSort.txt')
 quicksort(testList)
-print(testList)
+print(validate(testList))
