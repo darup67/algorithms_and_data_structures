6adf3582c5946578ce12980c192dd0a65a962f72
Matt Davis <matt_davis@me.com>
2016-12-13 17:20:42 -0600

    Python: min_cut working?

--- python/graph/min_cut.py
+++ python/graph/min_cut.py
@@ -1,28 +1,68 @@
+import random
+
+numVertices = 0
+
 def random_cut(g):
     ## Get random edge from list of edges
-    randEdge = g.edges[random.randrange(0, len(g.edges))]
+    randEdge = g[random.randrange(0, len(g))]
 
-    ## Connect all edges adjacent to randEdge[1] to randEdge[0]
-##    for x in range(0, len(g.vertices[randEdge[1]])
-##        vertexList = g.vertices[randEdge[1]]
-##        vertexList[x][0] = randEdge[0]
-                   
-    for edge in g.vertices[randEdge[1]]:
-        edge[0] = randEdge[0]
+    ## Connect all edges adjacent to randEdge[0] to randEdge[1]  
+    for edge in g:
+        if edge[0] == randEdge[0]:
+            edge[0] = randEdge[1]
+            
+        elif edge[1] == randEdge[0]:
+            edge[1] = randEdge[1]
 
-    g.vertices[randEdge[0]] += [ edge for edge in g.vertices[randEdge[1]] if edge[0] != edge[1] ]
-    g.vertices[randEdge[1]] = []
+    print('Before:')
+    for edge in g:
+        print(edge)
+    print('')
+    
+    g[:] = (edge for edge in g if edge[0] != edge[1] and not is_in_list(g, edge))
 
-    for x in range(0, len(g.edges))
-        
+    print('After:')
+    for edge in g:
+        print(edge)
+    print('')
+
 
 def min_cut(g):
-    remainingVertices = len(g.vertices)
-
-    while (remainingVertices > 2):
+    global numVertices
+    while (numVertices > 2):
         random_cut(g)
-        remainingVertices -= 1
+        numVertices -= 1
 
-    
+    return len(g)
 
-    
+
+def is_in_list(inputList, inputEdge):
+    for edge in inputList:
+        if edge[0] == inputEdge[1] and edge[1] == inputEdge[0]:
+            return True
+
+    return False
+
+
+def get_graph_from_file(filename):
+    global numVertices
+    newList, result = list(open(filename)), []
+
+    for item in newList:
+        numVertices += 1
+        numList = item.rstrip('\n').split(' ')
+
+        for edgeNum in numList[1:]:
+            edge = [numList[0], edgeNum]
+            if (not is_in_list(result, edge)):
+                result.append(edge)
+
+    return result
+
+graph = get_graph_from_file('data/min_cut_1.txt')
+for edge in graph:
+    print(edge)
+
+print('')
+print(min_cut(graph))
+
