bb9dde98a5d0b60b5a15ff25f999888746fd9d09
Matt Davis <matt_davis@me.com>
2016-12-13 21:40:02 -0600

    Python: min_cut needs more work

--- python/graph/min_cut.py
+++ python/graph/min_cut.py
@@ -1,13 +1,14 @@
 import random, copy
+from operator import itemgetter
 
-numVertices = 0
+class Graph: pass
 
 def random_cut(g):
     ## Get random edge from list of edges
-    randEdge = g[random.randrange(0, len(g))]
+    randEdge = g.edges[random.randrange(0, len(g.edges))]
 
     ## Connect all edges adjacent to randEdge[0] to randEdge[1]  
-    for edge in g:
+    for edge in g.edges:
         if edge[0] == randEdge[0]:
             edge[0] = randEdge[1]
             
@@ -15,80 +16,90 @@ def random_cut(g):
             edge[1] = randEdge[1]
 
 ##    print('Before:')
-##    for edge in g:
+##    for edge in g.edges:
 ##        print(edge)
 ##    print('')
     
-    g[:] = (edge for edge in g if edge[0] != edge[1])
+    g.edges[:] = (edge for edge in g.edges if edge[0] != edge[1])
+    for edge in g.edges:
+        if edge[0] > edge[1]:
+            edge[0], edge[1] = edge[1], edge[0]
 
 ##    print('After:')
-##    for edge in g:
+##    for edge in g.edges:
 ##        print(edge)
 ##    print('')
 
-    if len(g) < 3:
-        for edge in g:
+    if len(g.edges) < 3:
+        for edge in g.edges:
             print(edge)
         print('')
 
-
-def min_cut(g, n):
+def min_cut(g):
     minimum = 100000000;
 
     for x in range(0, 100):
-        temp_g, temp_n = copy.deepcopy(g), n
+        temp_g, n = copy.deepcopy(g), len(g.vertices) - 1
+        
+        while (n > 2):
+            random_cut(temp_g)
+            n -= 1
 
-##        for edge in temp_g:
+        temp_g.edges.sort(key=itemgetter(0,1))
+##        for edge in temp_g.edges:
 ##            print(edge)
 ##        print('')
-        
-        while (temp_n > 2):
-            random_cut(temp_g)
-            temp_n -= 1
 
-        unique = 0
-        for i in range(0, len(temp_g)):
-            for j in range (i + 1, len(temp_g)):
-                if temp_g[i][0] == temp_g[j][0] and temp_g[i][1] == temp_g[j][1]:
-                    break
-                elif temp_g[i][0] == temp_g[j][1] and temp_g[i][1] == temp_g[j][0]:
-                    break
-            else:
+        unique = 1
+        for x in range(0, len(temp_g.edges) - 1):
+            if temp_g.edges[x][0] != temp_g.edges[x+1][0] or temp_g.edges[x][1] != temp_g.edges[x+1][1]:
                 unique += 1
+        
+        if unique < 2:
+            for edge in temp_g.edges:
+                print(edge)
+            print('')
         
         if unique < minimum:
             minimum = unique
 
     return minimum
 
-
 def is_in_list(inputList, inputEdge):
     for edge in inputList:
-        if edge[0] == inputEdge[1] and edge[1] == inputEdge[0]:
+        if edge[0] == inputEdge[0] and edge[1] == inputEdge[1]:
             return True
 
     return False
 
-
 def get_graph_from_file(filename):
-    global numVertices
-    newList, result = list(open(filename)), []
+    newList, graph = list(open(filename)), Graph()
+    graph.vertices, graph.edges = [], []
+    
+    for x in range(0, len(newList) + 1):
+        graph.vertices.append([])
 
     for item in newList:
-        numVertices += 1
-        numList = item.rstrip('\n').split(' ')
-
+        numList = list(map(int, item.rstrip('\n').split(' ')))
+        
         for edgeNum in numList[1:]:
-            edge = [numList[0], edgeNum]
-            if (not is_in_list(result, edge)):
-                result.append(edge)
+            if numList[0] < edgeNum:
+                edge = [numList[0], edgeNum]
+            else:
+                edge = [edgeNum, numList[0]]
+            
+            if not is_in_list(graph.edges, edge):
+                graph.edges.append(edge)
+            
+            graph.vertices[numList[0]].append(edge)
 
-    return result
+    return graph
 
 graph = get_graph_from_file('data/min_cut_1.txt')
-##for edge in graph:
+##for vertex in graph.vertices:
+##    print(vertex)
+##for edge in graph.edges:
 ##    print(edge)
 ##print('')
 
-print(min_cut(graph, numVertices))
-
+print(min_cut(graph))
