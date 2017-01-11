b211f072180a7a91723294730fe25784b819d2b3
Matt Davis <matt_davis@me.com>
2017-01-11 16:11:49 -0600

    javascript: huffman added

---
+++ javascript/dynamic/max_weight_indep_set.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const testArr = [1, 2, 3, 4, 5];
+
+function huffmanMinMax (arr) {
+  const numChars = arr.length;
+  let nodes       = arr.sort((a,b) => b-a).map(e => { return { weight: e, parent: null } }),
+      minPriority = nodes[arr.length - 1],
+      maxPriority = nodes[0];
+
+  function countDepth (node, count=0) {
+    if (node.parent !== null) return countDepth(node.parent, count + 1);
+    return count;
+  }
+
+  for (let i = 1; i < numChars; i++) {
+    const left    = nodes.pop(),
+          right   = nodes[nodes.length - 1],
+          newNode = { weight: left.weight + right.weight, parent: null };
+
+    left.parent = right.parent = newNode;
+    nodes[nodes.length - 1] = newNode;
+    nodes.sort((a,b) => b.weight - a.weight);
+  }
+
+  return {minLength: countDepth(maxPriority), maxLength: countDepth(minPriority)};
+}
+
+
+// console.log(huffmanMinMax(testArr));
+
+fs.readFile('huffman.txt', 'utf8', (err, data) => {
+  const lines = data.split('\n').slice(1,-1).map(Number);
+
+  console.log(huffmanMinMax(lines));
+})
