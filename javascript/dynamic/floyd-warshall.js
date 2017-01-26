1c5a9dc9fb4c214e441288f825641daf283757fc
Matt Davis <matt_davis@me.com>
2017-01-26 08:59:08 -0600

    javascript: floyd-warshall working

--- javascript/dynamic/floyd-warshall.js
+++ javascript/dynamic/floyd-warshall.js
@@ -15,10 +15,10 @@ function floydWarshall (vertices) {
   }
 
   // Initialization step
-  for (let i = 0; i < vertices.length; i++) {
+  for (let i = 1; i < vertices.length; i++) {
     arr[i] = [];
 
-    for (let j = 0; j < vertices.length; j++) {
+    for (let j = 1; j < vertices.length; j++) {
       arr[i][j] = [];
       
       if (i === j) arr[i][j][0] = 0;
@@ -32,8 +32,8 @@ function floydWarshall (vertices) {
 
   // Main loop
   for (let k = 1; k < vertices.length; k++) {
-    for (let i = 0; i < vertices.length; i++) {
-      for (let j = 0; j < vertices.length; j++) {
+    for (let i = 1; i < vertices.length; i++) {
+      for (let j = 1; j < vertices.length; j++) {
         arr[i][j][k] = Math.min(arr[i][j][k - 1], arr[i][k][k - 1] + arr[k][j][k - 1]);
       }
     }
@@ -43,17 +43,17 @@ function floydWarshall (vertices) {
 }
 
 // Read file and process data set
-fs.readFile("data/graph1.txt", "utf8", (err, data) => {
-  const lines    = data.split('\n'),
+fs.readFile("data/test1.txt", "utf8", (err, data) => {
+  const lines    = data.split('\r\n'),
         vertices = new Array(parseInt(lines[0].split(' ')[0]));
 
   // Create vertices from file data
   // Renames vertices (vertex = vertex - 1) to align with zero indexed arrays
   for (let i = 1; i < lines.length - 1; i++) {
     const [startVertex, endVertex, weight] = lines[i].split(' ').map(Number);
-    vertices[startVertex - 1] ? vertices[startVertex - 1].push([endVertex - 1, weight]) : vertices[startVertex - 1] = [[endVertex - 1, weight]];
+    vertices[startVertex] ? vertices[startVertex].push([endVertex, weight]) : vertices[startVertex] = [[endVertex, weight]];
   }
   
-  const output = floydWarshall(testInput);
+  const output = floydWarshall(vertices);
   console.log(output);
 })
