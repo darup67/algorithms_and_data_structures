668634685069610bdc2b3c3d1b97c1851ecea509
Matt Davis <18077201+mattsplats@users.noreply.github.com>
2018-09-15 19:20:28 -0700

    new file structure

--- cpp/tree/binary_tree.cpp
+++ cpp/tree/binary_tree.cpp
@@ -1,70 +1,196 @@
-#include <iostream>
-#include <set>
+#include <iosfwd>
 
-#include "binary_tree.h"
+template <typename T>
+struct BinaryTree<T>::TreeNode {
+    std::unique_ptr<TreeNode> left{nullptr}, right{nullptr};
+    TreeNode* parent{nullptr};
+    const T val;
 
-using namespace std;
+    explicit TreeNode(const T v) : val(v) {}
+    explicit TreeNode(const T v, TreeNode *const p) : val(v), parent(p) {}
+};
 
+template <typename T>
+class BinaryTree<T>::const_iterator {
+    const TreeNode* node;
+    
+public:
+    using iterator_category = std::input_iterator_tag;
+    using value_type = const T;
+    using difference_type = const T;
+    using pointer = const T*;
+    using reference = const T&;
 
-int main(int argc, char const *argv[])
-{
-    BinaryTree<int> tree{20,10,30};
-    set<int> valueSet;
+    explicit const_iterator(TreeNode* n) : node(n) {}
 
-    tree.erase({10,30,20});
+    const_iterator& operator++();
+    const_iterator operator++(int);
+    bool operator==(const_iterator& other) const { return node == other.node; }
+    bool operator!=(const_iterator& other) const { return !(*this == other); }
+    reference operator*() const { return node->val; }
+};
 
-    if (!tree.empty()) {
-        cout << endl << "ERROR!" << endl;
-        cout << "Tree not empty, size: " << tree.size() << endl;
-        exit(1);
-    };
+template <typename T>
+typename BinaryTree<T>::const_iterator& BinaryTree<T>::const_iterator::operator++() {
+    // Node is the root or has a right child - descend to successor node
+    if (!node->parent || node->right) {
+        node = node->right.get();
+        while (node && node->left) node = node->left.get();
 
-    for (int i{0}; i < 10000; ++i) {
-        auto newVal = rand();
-        if (i % 3 == 2) {
-            auto it = valueSet.begin();
-            auto finalPos = newVal % valueSet.size();
-            for (; finalPos > 0; --finalPos) ++it;
-            // cout << "Deleting " << *it << endl;
-            tree.erase(*it);
-            valueSet.erase(it);
-        } else {
-            // cout << "Inserting " << newVal << endl;
-            tree.insert(newVal);
-            valueSet.insert(newVal);
-        }
+    // Node is on the left - ascend once
+    } else if (!node->parent->right || node->parent->right.get() != node) {
+        node = node->parent;
 
-        if (!equal(tree.begin(), tree.end(), valueSet.begin())) {
-            cout << endl << "ERROR! Iteration #" << i << endl;
-            
-            cout << "Expected: ";
-            for (int x : valueSet) cout << x << ", ";
-            cout << endl;
-            
-            cout << "Actual: " << endl;
-            int j{0};
-            for (int x : tree) {
-                cout << x << ", ";
-                ++j;
-                if (j > tree.size() + 10) break;
-            }
-
-            exit(1);
-        }
+    // Node is on the right
+    } else {
+        // Ascend to the parent of the root of this right branch
+        auto currVal{node->val};
+        while (node->parent && node->parent->right.get() == node) node = node->parent;
+        node = node->parent;
+        
+        // If we ascend to the root, and it is the current node (node == root) or less than the current node (node == endNode)
+        if (node && node->val <= currVal) node = nullptr;
     }
-
-    tree.clear();
-
-    if (!tree.empty()) {
-        cout << endl << "ERROR!" << endl;
-        cout << "Tree not empty, size: " << tree.size() << endl;
-        exit(1);
-    };
+    return *this;
 }
 
-/*
-TODO:
-Replace rand() with better random func in tests
-Add template specialization for key, value pairs
-Add iterator pre/postfix -- (support reverse iteration)
-*/
+template <typename T>
+typename BinaryTree<T>::const_iterator BinaryTree<T>::const_iterator::operator++(int) {
+    const_iterator retVal{*this};
+    ++(*this);
+    return retVal;
+}
+
+template <typename T>
+void BinaryTree<T>::printInOrder(const BinaryTree<T>::TreeNode *const node) const {
+    if (!node) return;
+    printInOrder(node->left.get());
+    std::cout << node->val << ", ";
+    printInOrder(node->right.get());
+}
+
+template <typename T>
+typename BinaryTree<T>::TreeNode *const BinaryTree<T>::find(const T val, BinaryTree<T>::TreeNode *const node) const {
+    if (!node)            { return nullptr; }
+    if (node->val == val) { return node; }
+    if (node->val > val)  { return find(val, node->left.get()); }
+    return find(val, node->right.get());
+}
+
+template <typename T>
+void BinaryTree<T>::insert(const T val, TreeNode *const node) {
+    if (node->val > val) {
+        if (!node->left) {
+            node->left = std::make_unique<TreeNode>(val, node);
+            ++_size;
+        } else {
+            insert(val, node->left.get());
+        }
+    } else if (node->val < val) {
+        if (!node->right) {
+            node->right = std::make_unique<TreeNode>(val, node);
+            ++_size;
+        } else {
+            insert(val, node->right.get());
+        }
+    }
+}
+
+template <typename T>
+typename BinaryTree<T>::TreeNode *const BinaryTree<T>::erase(const T val, BinaryTree<T>::TreeNode *const node) {
+    if (!node) { return nullptr; }
+    if (node->val == val) {
+        if (_size) --_size;
+
+        // Zero children: return null
+        if (!node->left && !node->right) return nullptr;
+
+        // One child: return the non-null child
+        if (!node->left)  return node->right.release();
+        if (!node->right) return node->left.release();
+
+        // Two children: Hibbard deletion
+        // First, find successor node
+        TreeNode* prev{node};
+        TreeNode* replacement{node->right.get()};
+        while (replacement->left) {
+            prev = replacement;
+            replacement = replacement->left.get();
+        }
+
+        // If successor == node.right, only replace left subtree
+        if (prev == node) {
+            replacement = node->right.release();
+            replacement->left = std::unique_ptr<TreeNode>(node->left.release());
+            if (replacement->left) replacement->left->parent = replacement;
+
+            return replacement;
+        }
+
+        // Else, replace both subtrees and repair prev--x-->repl->right to prev->right
+        replacement = prev->left.release();
+        prev->left = std::unique_ptr<TreeNode>(replacement->right.release());
+        if (prev->left) prev->left->parent = prev;
+
+        replacement->left = std::unique_ptr<TreeNode>(node->left.release());
+        replacement->right = std::unique_ptr<TreeNode>(node->right.release());
+        if (replacement->left) replacement->left->parent = replacement;
+        if (replacement->right) replacement->right->parent = replacement;
+
+        return replacement;
+    }
+    if (node->val > val) {
+        auto retNode{erase(val, node->left.get())};
+        if (retNode != node->left.get()) {
+            if (retNode) retNode->parent = node;
+            node->left = std::unique_ptr<TreeNode>(retNode);
+        }
+    } else {
+        auto retNode{erase(val, node->right.get())};
+        if (retNode != node->right.get()) {
+            if (retNode) retNode->parent = node;
+            node->right = std::unique_ptr<TreeNode>(retNode);
+        }
+    }
+    return node;
+}
+
+template <typename T>
+void BinaryTree<T>::print() const {
+    printInOrder(root.get());
+    std::cout << std::endl;
+}
+
+template <typename T>
+void BinaryTree<T>::insert(const T val) {
+    if (!root) {
+        root = std::make_unique<TreeNode>(val);
+        ++_size;
+    } else {
+        insert(val, root.get());
+    }
+}
+
+template <typename T>
+void BinaryTree<T>::erase(const T val) {
+    TreeNode *const newRoot = erase(val, root.get());
+    if (newRoot != root.get()) {
+        if (newRoot) newRoot->parent = nullptr;
+        root = std::unique_ptr<TreeNode>(newRoot);
+    }
+};
+
+template <typename T>
+void BinaryTree<T>::clear() {
+    root.reset();
+    _size = 0;
+}
+
+template <typename T>
+const typename BinaryTree<T>::const_iterator BinaryTree<T>::begin() const {
+    if (empty()) return _end;
+
+    auto retNode{root.get()};
+    while (retNode->left) retNode = retNode->left.get();
+    return const_iterator(retNode);
+}
---
+++ cpp/tree/binary_tree_test.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <set>
+
+#include "binary_tree.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    BinaryTree<int> tree{20,10,30};
+    set<int> valueSet;
+
+    tree.erase({10,30,20});
+
+    if (!tree.empty()) {
+        cout << endl << "ERROR!" << endl;
+        cout << "Tree not empty, size: " << tree.size() << endl;
+        exit(1);
+    };
+
+    for (int i{0}; i < 10000; ++i) {
+        auto newVal = rand();
+        if (i % 3 == 2) {
+            auto it = valueSet.begin();
+            advance(it, newVal % valueSet.size());
+            // cout << "Deleting " << *it << endl;
+            tree.erase(*it);
+            valueSet.erase(it);
+        } else {
+            // cout << "Inserting " << newVal << endl;
+            tree.insert(newVal);
+            valueSet.insert(newVal);
+        }
+
+        if (!equal(tree.begin(), tree.end(), valueSet.begin())) {
+            cout << endl << "ERROR! Iteration #" << i << endl;
+            
+            cout << "Expected: ";
+            for (int x : valueSet) cout << x << ", ";
+            cout << endl;
+            
+            cout << "Actual: " << endl;
+            tree.print();
+
+            exit(1);
+        }
+    }
+
+    tree.print();
+    tree.clear();
+
+    if (!tree.empty()) {
+        cout << endl << "ERROR!" << endl;
+        cout << "Tree not empty, size: " << tree.size() << endl;
+        exit(1);
+    };
+}
+
+/*
+TODO:
+Replace rand() with better random func
+*/
